f207285527f5acc0ce89c8067e41ebd56ea21cac
unproduktif <dodiwijayabusiness@gmail.com>
2026-06-08 22:53:35 +0800

    Update GeminiResponse

--- app/src/main/java/com/example/kaktuan/api/GeminiResponse.kt
+++ app/src/main/java/com/example/kaktuan/api/GeminiResponse.kt
@@ -1,2 +1,17 @@
 package com.example.kaktuan.api
 
+data class GeminiResponse(
+    val candidates: List<Candidate>?
+)
+
+data class Candidate(
+    val content: ContentResponse?
+)
+
+data class ContentResponse(
+    val parts: List<PartResponse>?
+)
+
+data class PartResponse(
+    val text: String?
+)
